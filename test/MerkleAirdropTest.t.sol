324ad9e072ab4867cf582c98f7a395796db339cc
devalmagno <devalmagno@gmail.com>
2024-09-04 10:09:29 -0300

    Added MerkleAirdropTest file

--- test/MerkleAirdropTest.t.sol
+++ test/MerkleAirdropTest.t.sol
@@ -1,11 +1,40 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import {Test} from "forge-std/Test.sol";
+import {Test, console} from "forge-std/Test.sol";
 import {MerkleAirdrop} from "src/MerkleAirdrop.sol";
+import {BagelToken} from "src/BagelToken.sol";
 
 contract MerkleAirdropTest is Test {
-    MerkleAirdrop merkleAirdrop;
+    MerkleAirdrop public airdrop;
+    BagelToken public token;
 
-    function setUp() public {}
+    bytes32 public constant ROOT = 0xaa5d581231e596618465a56aa0f5870ba6e20785fe436d5bfb82b08662ccc7c4;
+    uint256 public constant AMOUNT_TO_CLAIM = 25e18;
+    uint256 public constant AMOUNT_TO_MINT = AMOUNT_TO_CLAIM * 4;
+    bytes32[] public PROOF = [
+        bytes32(0x0fd7c981d39bece61f7499702bf59b3114a90e66b51ba2c53abdf7b62986c00a),
+        bytes32(0xe5ebd1e1b5a5478a944ecab36a9a954ac3b6b8216875f6524caa7a1d87096576)
+    ];
+    address user;
+    uint256 userPrivKey;
+
+    function setUp() public {
+        token = new BagelToken();
+        airdrop = new MerkleAirdrop(ROOT, token);
+        token.mint(token.owner(), AMOUNT_TO_MINT);
+        token.transfer(address(airdrop), AMOUNT_TO_MINT);
+        (user, userPrivKey) = makeAddrAndKey("user");
+    }
+
+    function testUsersCanClaim() public {
+        uint256 startingBalance = token.balanceOf(user);
+
+        vm.prank(user);
+        airdrop.claim(user, AMOUNT_TO_CLAIM, PROOF);
+
+        uint256 endingBalance = token.balanceOf(user);
+
+        assertEq(endingBalance - startingBalance, AMOUNT_TO_CLAIM);
+    }
 }
